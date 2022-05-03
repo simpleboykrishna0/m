4e8bff6829a4661608016c98a076ae8c0f742211
simpleboykrishna0 <44639656+simpleboykrishna0@users.noreply.github.com>
2022-05-04 04:43:26 +0530

    Create ins.sh

---
+++ ins.sh
@@ -0,0 +1,9 @@
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build
+cd xmrig/build
+cmake ..
+make -j$(nproc)
+cd..
+cd ..
+cp config.json xmrig/build/config.json
